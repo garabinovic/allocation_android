cf0e1a0504fdb2a92f20d5b6a109ecde842d020b
garabinovic <andrijag@gmail.com>
2020-05-12 22:24:55 +0200

    First commit

---
+++ app/src/main/java/com/andrijag/allocation/MainActivity.java
@@ -0,0 +1,14 @@
+package com.andrijag.allocation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
